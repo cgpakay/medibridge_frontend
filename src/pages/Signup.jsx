54c350c38d0da2fe453fc429d691b802c6add1aa
Jonas Ienco <jonasienco1@gmail.com>
2024-05-29 13:55:19 +0200

    restyled signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -186,13 +186,7 @@ const Signup = () => {
               </div>
 
               <div className="mb-5 flex items-center gap-3">
-                <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
-                  <img
-                    src={previewURL || avatar}
-                    alt="avatar"
-                    className="w-full rounded-full"
-                  />
-                </figure>
+                <figure className=""></figure>
 
                 <div className="relative w-[130px] h-[50px]">
                   <input
@@ -207,7 +201,7 @@ const Signup = () => {
                     htmlFor="customFile"
                     className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer"
                   >
-                    Upload Photo
+                    Upload Files
                   </label>
                 </div>
               </div>
