8892b8a9dd67de8a0a7f07bf1abf02ff6af7c2a3
Jonas Ienco <jonasienco1@gmail.com>
2024-05-29 13:53:00 +0200

    signup page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Testimonials from "../components/Testimonials";
 import OurServices from "../components/OurServices";
+import Signup from "./Signup";
 
 const Home = () => {
   return (
     <div>
       <OurServices />
       <Testimonials />
+      <Signup />
     </div>
   );
 };
