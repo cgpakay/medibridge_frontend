ae6002c091c166cb509245a5a49b7b9c0726659e
OpheliaDanso <mayaafreh@gmail.com>
2024-06-05 16:52:17 +0200

    still on fetch data

--- src/hooks/useFetchData.jsx
+++ src/hooks/useFetchData.jsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import { token } from "../config";
+/* import { toast } from "react-toastify"; */
 
 const UseFetchData = (url) => {
   const [data, setData] = useState([]);
